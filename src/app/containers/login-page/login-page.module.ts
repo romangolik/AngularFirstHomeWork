b7e1e2635d7817f1bdfeb9a23d3f9769dc975aeb
Roman Golik <romangolik86786@gmail.com>
2020-12-03 15:48:07 +0200

    Add small validation

--- src/app/containers/login-page/login-page.module.ts
+++ src/app/containers/login-page/login-page.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page.component';
 import { LoginPageRouting } from './login-page.routing';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../rest/auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ LoginPageComponent ],
   imports: [
     FormsModule,
     CommonModule,
-    LoginPageRouting
+    LoginPageRouting,
+    HttpClientModule,
   ],
+  providers: [ AuthService ],
   exports: [ LoginPageComponent ]
 })
 export class LoginPageModule { }
